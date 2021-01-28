9b57548f1fe79243211e2722289d57005d933d5c
Erick Jensen <Erick.Jensen010@gmail.com>
2021-01-28 08:42:28 -0600

    add section for a company video

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,31 +1,32 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    /* Navigation scroll */
-    $(function() {
-        $('a[href*=#]:not([href=#])').click(function() {
-          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-              $('html,body').animate({
-                scrollTop: target.offset().top
-              }, 1000);
-              return false;
-            }
-          }
-        });
-      });
-
-      /* For the sticky navigation */
-    $('.js--company-info').waypoint(function(direction) {
-        if (direction == "down") {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
+  /* Navigation scroll */
+  $(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
         }
-    }, {
-      offset: '60px;'
+      }
     });
+  });
+
+  /* For the sticky navigation */
+  $('.js--company-video').waypoint(function (direction) {
+    if (direction == "down") {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
+  }, {
+    offset: '80px;'
+  });
+
 
 
 
